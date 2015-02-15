acb026998854257f0bc30e4847033cd413301d7b
Daniel Delgado <dedd712@gmail.com>
2015-02-14 23:27:44 -0430

    Cambiado favicon y agregado texto falso, a falta de escribir algún mensaje de portada y darle nombres a los links

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="es"><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+    <link rel="icon" href="ico/favicon.ico">
 
     <title>MultibazarLRD</title>
 
